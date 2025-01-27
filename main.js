3b29ec11c36122ef0a3b2d1f5c15921029a64936
Dilshod2010 <dilshodjon.fahriddinov@gmail.com>
2025-01-27 10:09:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// const username = prompt("Enter name")
+// const newTag = document.createElement("h1");
+// newTag.textContent = username;
+
+// newTag.style.backgroundColor = "red";
+// newTag.style.textAlign = "center";
+// newTag.style.color = "white";
+
+
+// document.body.appendChild(newTag);
+
+// const ranglar = prompt("ranglarni yozing");
+
+// const newTag = document.createElement("h1");
+// newTag.textContent = ranglar;
+
+// document.body.style.backgroundColor = ""
+
+// document.body.appendChild(newTag);
+
+
+
+// // const width = prompt("razmer kiriting");
+// // const height = prompt("2razmer kiriting");
+// // const ranglar = prompt("rang kiriting");
+
+
+// // const newTag = document.createElement("div");
+// // newTag.style.width = `${width}px`;
+// // newTag.style.height = `${height}px`;
+// // newTag.style.ranglar = `${bg}`;
+
+// // document.body.appendChild(newTag);
+
+
+
+const createElement= prompt("xohlagan sozingizni kiriting ")
+const newTag = document.createElement("h1");
+
+newTag.textContent = createElement;
+
+newTag.style.textAlign = "center";
+newTag.style.alignItems = "center";
+newTag.style.color = "yellow";
+newTag.style.fontSize = "100px"
+
+document.body.appendChild(newTag);
+
+let color = prompt("xohlagan rang yozing");
+document.body.style.background = color;
+
+
+
+
+
+
+
